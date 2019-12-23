066b00393d7687bd1726e1e15ae555e811456dbe
wsli <2358130546@qq.com>
2019-12-23 23:05:35 +0800

    .gitignore update

---
+++ myNetty/src/main/java/com/MyNetty/MyNettyApplication.java
@@ -0,0 +1,11 @@
+package com.MyNetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNettyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyNettyApplication.class, args);
+    }
+}
---
+++ myNetty/src/main/java/com/MyNetty/myTest/MyNettyClientHandler.java
@@ -0,0 +1,115 @@
+package com.MyNetty.myTest;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+public class MyNettyClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    protected MyNettyClientHandler() {
+        super();
+    }
+
+    protected MyNettyClientHandler(boolean autoRelease) {
+        super(autoRelease);
+    }
+
+    protected MyNettyClientHandler(Class<? extends ByteBuf> inboundMessageType) {
+        super(inboundMessageType);
+    }
+
+    protected MyNettyClientHandler(Class<? extends ByteBuf> inboundMessageType, boolean autoRelease) {
+        super(inboundMessageType, autoRelease);
+    }
+
+    @Override
+    public boolean acceptInboundMessage(Object msg) throws Exception {
+        return super.acceptInboundMessage(msg);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        super.channelRead(ctx, msg);
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        super.channelUnregistered(ctx);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush("新的客户端进来");
+
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String line = "";
+        while ((line = bufferedReader.readLine()) != null){
+            ctx.writeAndFlush(line);
+        }
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("客户端与服务端通道-关闭：" + ctx.channel().localAddress() + "channelInactive");
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        super.channelReadComplete(ctx);
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        super.userEventTriggered(ctx, evt);
+    }
+
+    @Override
+    public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+        super.channelWritabilityChanged(ctx);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+        System.out.println("异常退出:" + cause.getMessage());
+    }
+
+    @Override
+    protected void ensureNotSharable() {
+        super.ensureNotSharable();
+    }
+
+    @Override
+    public boolean isSharable() {
+        return super.isSharable();
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        super.handlerAdded(ctx);
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        super.handlerRemoved(ctx);
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
+        System.out.println("读取客户端通道信息..");
+        ByteBuf buf = byteBuf.readBytes(byteBuf.readableBytes());
+        System.out.println("客户端接收到的服务端信息:" + ByteBufUtil.hexDump(buf) + "; 数据包为:" + buf.toString(Charset.forName("utf-8")));
+    }
+}
